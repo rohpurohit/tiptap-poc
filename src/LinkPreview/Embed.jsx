b80cd81ff06a11464ef8144a058b83b60b966699
rohan <rohan.purohit@coditation.com>
2022-06-15 09:55:53 +0530

    update embed node

--- src/LinkPreview/Embed.jsx
+++ src/LinkPreview/Embed.jsx
@@ -51,62 +51,8 @@ export default Node.create({
     ];
   },
 
-  // component({ isEditable, isSelected, theme, node }) {
-  //   const { embeds } = this.editor.props;
-
-  //   // matches are cached in module state to avoid re running loops and regex
-  //   // here. Unfortunately this function is not compatible with React.memo or
-  //   // we would use that instead.
-  //   const hit = cache[node.attrs.href];
-  //   let Component = hit ? hit.Component : undefined;
-  //   let matches = hit ? hit.matches : undefined;
-  //   let embed = hit ? hit.embed : undefined;
-
-  //   if (!Component) {
-  //     for (const e of embeds) {
-  //       const m = e.matcher(node.attrs.href);
-  //       if (m) {
-  //         Component = e.component;
-  //         matches = m;
-  //         embed = e;
-  //         cache[node.attrs.href] = { Component, embed, matches };
-  //       }
-  //     }
-  //   }
-
-  //   if (!Component) {
-  //     return null;
-  //   }
-
-  //   return (
-  //     <Component
-  //       attrs={{ ...node.attrs, matches }}
-  //       isEditable={isEditable}
-  //       isSelected={isSelected}
-  //       theme={theme}
-  //     />
-  //   );
-  // }
-
-  // addCommands() {
-  //   return {
-  //     embed:
-  //       () =>
-  //       ({ commands }) => {
-  //         return commands.setNode("paragraph");
-  //       },
-  //   };
-  // }
-  // commands({ type }) {
-  //   return (attrs) => (state, dispatch) => {
-  //     dispatch(
-  //       state.tr.replaceSelectionWith(type.create(attrs)).scrollIntoView()
-  //     );
-  //     return true;
-  //   };
-  // },
   addNodeView() {
-    return ReactNodeViewRenderer(Figma);
+    return ReactNodeViewRenderer(Preview);
   },
   addCommands() {
     return {
