e849f5801c4398bb0e97cc304f587ae52e4a8f97
rohan <rohan.purohit@coditation.com>
2022-06-22 10:30:35 +0530

    update collab cursor

--- src/Realtime.js
+++ src/Realtime.js
@@ -12,17 +12,97 @@ import * as Y from "yjs";
 
 const ydoc = new Y.Doc();
 const provider = new WebsocketProvider(
-  "wss://demos.yjs.dev",
+  // "ws:52.13.17.64:1234",
+  "ws:localhost:4444",
   "tiptap-demo",
   ydoc
 );
 const type = ydoc.getXmlFragment("prosemirror");
+const awarenessStatesToArray = (states) => {
+  return Array.from(states.entries()).map(([key, value]) => {
+    return {
+      clientId: key,
+      ...value.user,
+    };
+  });
+};
+const defaultOnUpdate = () => null;
+
 const Realtime = Extension.create({
   name: "realtime",
+  addOptions() {
+    return {
+      user: {
+        name: null,
+        color: null,
+      },
+      render: (user) => {
+        const cursor = document.createElement("span");
+
+        cursor.classList.add("collaboration-cursor__caret");
+        cursor.setAttribute("style", `border-color: ${user.color}`);
+
+        const label = document.createElement("div");
+
+        label.classList.add("collaboration-cursor__label");
+        label.setAttribute("style", `background-color: ${user.color}`);
+        label.insertBefore(document.createTextNode(user.name), null);
+        cursor.insertBefore(label, null);
+
+        return cursor;
+      },
+      onUpdate: defaultOnUpdate,
+    };
+  },
+  addStorage() {
+    return {
+      users: [],
+    };
+  },
+
+  addCommands() {
+    return {
+      updateUser: (attributes) => () => {
+        this.options.user = attributes;
+
+        provider.awareness.setLocalStateField("user", this.options.user);
+
+        return true;
+      },
+      user:
+        (attributes) =>
+        ({ editor }) => {
+          console.warn(
+            '[tiptap warn]: DEPRECATED: The "user" command is deprecated. Please use "updateUser" instead. Read more: https://tiptap.dev/api/extensions/collaboration-cursor'
+          );
+
+          return editor.commands.updateUser(attributes);
+        },
+    };
+  },
   addProseMirrorPlugins() {
     return [
       ySyncPlugin(type),
-      yCursorPlugin(provider.awareness),
+      yCursorPlugin(
+        (() => {
+          provider.awareness.setLocalStateField("user", this.options.user);
+
+          this.storage.users = awarenessStatesToArray(
+            provider.awareness.states
+          );
+
+          provider.awareness.on("update", () => {
+            this.storage.users = awarenessStatesToArray(
+              provider.awareness.states
+            );
+          });
+
+          return provider.awareness;
+        })(),
+        {
+          cursorBuilder: this.options.render,
+        }
+      ),
       yUndoPlugin(),
       keymap({
         "Mod-z": undo,
